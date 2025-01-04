d933dc1fa75021500a93a83d42d764a44f3ccf59
Tom <martin07tom@gmail.com>
2025-01-04 04:30:25 +0100

    Added legals pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import Legal from '../views/Legals/LegalNotice.vue';
+import Privacy from '../views/Legals/Privacy.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/legal',
+    name: 'Legal',
+    component: Legal,
+  },
+  {
+    path: '/privacy',
+    name: 'Privacy',
+    component: Privacy,
   },
 ];
 
---
+++ src/views/Legals/LegalNotice.vue
@@ -0,0 +1,39 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
+import LegalContent from '@/components/Legals/LegalContent.vue';
+</script>
+
+<template>
+  <div class="page">
+    <Navbar class="navbar" />
+    <div class="legalPlacement">
+      <LegalContent />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+.page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .navbar {
+    z-index: 2;
+    position: fixed;
+    top: 3rem;
+  }
+
+  .legalPlacement {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    padding: 13rem 0;
+  }
+}
+
+</style>
